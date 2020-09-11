f8cb3e2c1fa835d58517d3b235dc52e7c222bab5
resolritter <joao.paulo.silvasouza@hotmail.com>
2020-09-11 12:01:55 -0300

    don't shadow the outer loop counter during insertion sort

--- js/insertion_sort.js
+++ js/insertion_sort.js
@@ -10,13 +10,15 @@ console.log("Before", before)
 
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    const current = arr[i]
-    while (i > 0 && arr[i - 1] > current) {
-      arr[i] = arr[i - 1]
-      i -= 1
+    let j = i
+    const current = arr[j]
+    while (j > 0 && arr[j - 1] > current) {
+      arr[j] = arr[j - 1]
+      j -= 1
     }
-    arr[i] = current
+    arr[j] = current
   }
+
   return arr
 }
 
