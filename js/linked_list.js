295a26ddc2fbe363787b420100d6f2d1a658b807
resolritter <joao.paulo.silvasouza@hotmail.com>
2020-09-16 09:26:41 -0300

    rework 'reverse_singly_linked_list.js' into the more general 'linked_list.js' add filtering to 'linked_list.js'

--- js/linked_list.js
+++ js/linked_list.js
@@ -38,7 +38,7 @@ function appendCurried(head) {
 }
 
 function reverse(head) {
-  function iter(current, value, isFirst = true) {
+  function iter(current, previousValue, isFirst = true) {
     if (current.next) {
       const [cell, lastValue] = iter(current.next, current.value, false)
       if (isFirst) {
@@ -48,7 +48,7 @@ function reverse(head) {
     } else {
       return [
         {
-          value,
+          value: previousValue,
         },
         current.value,
       ]
@@ -59,6 +59,41 @@ function reverse(head) {
   return result
 }
 
+function filter(head, value) {
+  let prev = undefined
+  let current = head
+
+  while (true) {
+    if (current.value === value) {
+      if (prev) {
+        prev.next = current.next
+      } else {
+        head = current.next
+      }
+    }
+    prev = current
+    if (!(current = current.next)) {
+      break
+    }
+  }
+  if (head.value === value) {
+    head = {
+      value: null,
+      next: null
+    }
+  }
+
+  return head
+}
+
 lines.forEach(appendCurried(head))
+
+console.log("--- original")
+forEach(head, console.log)
+
 const reversed = reverse(head)
+console.log("\n--- reversed")
 forEach(reversed, console.log)
+
+console.log("\n--- value '5' filtered out")
+forEach(filter(reversed, "5"), console.log)
